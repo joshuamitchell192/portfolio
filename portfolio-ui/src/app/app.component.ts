4523921fa0d26fcc5972bb548501babf3f7ae297
joshua mitchell <josh23797@gmail.com>
2022-08-13 23:30:53 +1000

    Initial layout created

--- portfolio-ui/src/app/app.component.ts
+++ portfolio-ui/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './shared/layout/navbar/navbar.component';
+import { FooterComponent } from './shared/layout/footer/footer.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [RouterModule, CommonModule, NavbarComponent]
+  imports: [RouterModule, CommonModule, NavbarComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'portfolio-ui';
--- portfolio-ui/src/routes.ts
+++ portfolio-ui/src/routes.ts
@@ -1,9 +1,11 @@
 import {Routes} from '@angular/router';
 import { HomeComponent } from './app/home/home.component';
+import { WorkComponent } from './app/work/work.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent, data: { animation: "home" }},
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'work', component: WorkComponent }
 ]
 
 export default routes;
